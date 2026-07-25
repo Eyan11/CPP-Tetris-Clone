140d85efaef2a2268209e7aff82cbd4993d52fd3
Eyan <eyanmartucci11@gmail.com>
2026-07-25 10:05:44 -0700

    asset: changed block colors
    - changed block colors

--- Tetris/Colors.cpp
+++ Tetris/Colors.cpp
@@ -1,17 +1,16 @@
 #include "Colors.h"
 
 const Color darkGrey = { 26, 31, 40, 255 }; // Empty cell
-const Color green = { 47, 230, 23, 255 };
-const Color red = { 232, 18, 18, 255 };
-const Color orange = { 226, 116, 17, 255 };
-const Color yellow = { 237, 234, 4, 255 };
-const Color purple = { 166, 0, 247, 255 };
-const Color cyan = { 21, 204, 209, 255 };
-const Color blue = { 13, 64, 216, 255 };
-const Color lightBlue = { 59, 85, 162, 255 };
-const Color darkBlue = { 44, 44, 127, 255 };
+
+const Color orange = { 247, 158, 0, 255 };
+const Color blue = { 40, 168, 247, 255 };
+const Color white = { 255, 241, 232, 255 };
+const Color yellow = { 247, 229, 38, 255 };
+const Color green = { 0, 221, 52, 255 };
+const Color pink = { 247, 115, 163, 255 };
+const Color red = { 247, 0, 75, 255 };
 
 std::vector<Color> GetCellColors()
 {
-	return { darkGrey , green, red, orange, yellow, purple, cyan, blue };
+	return { darkGrey, orange, blue, white, yellow, green, pink, red };
 }
--- Tetris/Colors.h
+++ Tetris/Colors.h
@@ -5,15 +5,12 @@
 
 // Extern = Global Variable. It lets us declare a variable in one file and use it in another
 extern const Color darkGrey; // Empty cell
-extern const Color green;
-extern const Color red;
 extern const Color orange;
-extern const Color yellow;
-extern const Color purple;
-extern const Color cyan;
 extern const Color blue;
-
-extern const Color lightBlue; // UI
-extern const Color darkBlue; // UI
+extern const Color white;
+extern const Color yellow;
+extern const Color green;
+extern const Color pink;
+extern const Color red;
 
 std::vector<Color> GetCellColors();
